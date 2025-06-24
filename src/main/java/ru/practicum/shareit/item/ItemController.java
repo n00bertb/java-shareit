5bfd8b4dd92ee2cf6a888de6835fce80bc6589b4
Артём Самуйлов <artemka.samuiloff@yandex.ru>
2025-06-24 15:02:04 +0300

    правки кодстаила

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.comment.CommentDto;
